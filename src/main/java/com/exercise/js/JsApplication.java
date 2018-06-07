4322adf0a13e75051bdb7116fbc8ca4740e6c7eb
tianbohao <unicode1027@163.com>
2018-06-07 08:56:08 +0800

    初始化

---
+++ src/main/java/com/exercise/js/JsApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/exercise/js/JsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exercise.js;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
